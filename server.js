75edd58c4835ffd92fc027d75167b2b8db705438
linhvn0110 <linhvn0110@yahoo.com>
2018-07-06 17:54:40 +0700

    Initialize repo

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleWare = {
+    requireAuthentication: function (req, res, next) {
+        console.log('private route hit!');
+        next();
+    },
+    logger: function (req, res, next) {
+        console.log('[' + new Date().toString() + '] Request: ' + req.method + ' ' + req.originalUrl);
+        next();
+    }
+};
+app.use(middleWare.logger);
+//app.use(middleWare.requireAuthentication);
+
+app.get('/about', middleWare.requireAuthentication, function (req, res) {
+    res.send('About Us!');
+});
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function () {
+    console.log('Express server started on port ' + PORT + '!');
+});
